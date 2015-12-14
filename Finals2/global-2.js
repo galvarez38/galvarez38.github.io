8145d605734f209e2c374b292cd286efff1b1b9e
galvarez38 <gabea03@gmail.com>
2015-12-14 17:18:56 -0500

    Update global-2.js

--- Finals2/global-2.js
+++ Finals2/global-2.js
@@ -674,7 +674,7 @@ var now;
         // log it out
         console.log(mytime.format());
         // append the time to the ny span -- formatting documention at http://momentjs.com/docs/#/displaying/
-        //$('button').append(" " + mytime.format('MMMM Do YYYY, h:mm:ss a'));
+        $().append(" " + mytime.format('MMMM Do YYYY, h:mm:ss a'));
         // convert the current time to la timezone
 		// JavaScript Document
 		
